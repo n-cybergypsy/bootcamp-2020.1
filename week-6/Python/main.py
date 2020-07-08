f2b58cb08e0d08ed64b03a53b1f3932c52b96b16
Chioma Nzeduru <con241@nyu.edu>
2020-07-08 17:25:50 -0400

    Mirror function

--- week-6/Python/main.py
+++ week-6/Python/main.py
@@ -14,9 +14,31 @@ def leaf_to_root(root, arr):
     leaf_to_root(root.right, arr)
 
 
+def mirror(root):
+
+    if root:
+        # Swaps left and right nodes.
+        temp = root.left
+        root.left = root.right
+        root.right = temp
+        # Swaps left and right nodes of children recursively.
+        mirror(root.left)
+        mirror(root.right)
+    
+    return root
+
+
+
 nodes = [1, 2, 3, 4, 5, 6, 7, None, None, None, None, 8, 9]
 tree = build(nodes)
 
 print(tree)
 arr = []
 leaf_to_root(tree, arr)
+
+node_2 = [1, 2, 3, 4, 5, 6, 7]
+second = build(node_2)
+
+print(second)
+print(mirror(second))
+
