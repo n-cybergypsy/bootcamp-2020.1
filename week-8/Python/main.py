0f844ffa42d202f7fe5519e40d9a2518b956e9e3
n-cybergypsy <59662647+n-cybergypsy@users.noreply.github.com>
2020-07-22 15:56:56 -0400

    Corrected solution to prompt 1

--- week-8/Python/main.py
+++ week-8/Python/main.py
@@ -1,28 +1,31 @@
 #Prompt 1 (Stone's Love)
+import bisect
+# Write your code here
+
 first = input()
 N = input()
-Q = input()
 N = N.split()
+Q = input()
 Q = Q.split()
+arr = []
+total = 0
 
+for i in N:
+    if i != ' ':
+        total += int(i)
+        arr.append(total)
 
-for i in range(len(Q)):
-    total = 0
-    for j in range(len(N)):
-        total += int(N[j])
-        if total >= int(Q[i]):
-            print(j+1)
-            break
-            
+for j in range(0,len(Q)): 
+    print((bisect.bisect_left(arr, int(Q[j]))+1))
             
 # Prompt 2 (Repeated K times)
-N = int(input())
-arr = input()
-arr = arr.split()
-b = sorted(set(arr))
-k = int(input())
+# N = int(input())
+# arr = input()
+# arr = arr.split()
+# b = sorted(set(arr))
+# k = int(input())
 
-for i in b:
-    if arr.count(i) == k:
-        print(i)
-        break
+#for i in b:
+#    if arr.count(i) == k:
+#        print(i)
+#        break
